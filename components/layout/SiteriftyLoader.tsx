167f20ee29fbdbe31e46aa31865c3e2c333e60ad
franknkiwanink-hub <franknkiwanink@gmail.com>
2026-07-20 13:55:59 +0200

    Production Pipeline Update

--- components/layout/SiteriftyLoader.tsx
+++ components/layout/SiteriftyLoader.tsx
@@ -67,8 +67,8 @@ export default function SiteriftyLoader() {
       <div className="s-nav">
         <a href="/" className="brand">
           <img
-            src="https://cdn.phototourl.com/member/2026-07-19-ffcaa670-d57c-44f6-8415-ab73856860b2.png"
-            alt="Siterifty.com"
+            src="/images/siterifty-logo.png"
+            alt="Siterifty.com — Buy, Sell, Build, Trust"
             style={{ height: "1.3rem", display: "block" }}
           />
         </a>
